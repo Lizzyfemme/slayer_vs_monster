4c7dc7f52def5da8d6ca9864413aad5a38e36753
Lizzyfemme <lizzy_swann@hotmail.com>
2020-04-26 14:37:06 -0700

    Monster and slayer bars reflex data

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+A simple project created while learning VueJS
---
+++ app.js
@@ -0,0 +1,8 @@
+new Vue({
+  el: "#app",
+  data: {
+    slayerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+  },
+});
