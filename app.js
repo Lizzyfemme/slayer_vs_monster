28cc928e0e36e6e35a76407984a31d65e3841e2a
Lizzyfemme <lizzy_swann@hotmail.com>
2020-04-26 21:35:09 -0700

    Attack button working

--- app.js
+++ app.js
@@ -11,5 +11,42 @@ new Vue({
       this.slayerHealth = 100;
       this.monsterHealth = 100;
     },
+    attack: function () {
+      const slayerMinDamage = 3;
+      const slayerMaxDamage = 10;
+      const monsterMinDamage = 5;
+      const monsterMaxDamage = 12;
+
+      this.monsterHealth -= this.damage(slayerMinDamage, slayerMaxDamage);
+      if (this.checkWin()) {
+        return;
+      }
+      this.slayerHealth -= this.damage(monsterMinDamage, monsterMaxDamage);
+      this.checkWin();
+    },
+    specialAttack: function () {},
+    heal: function () {},
+    giveUp: function () {},
+    damage: function (minDamage, maxDamage) {
+      return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.slayerHealth <= 0) {
+        if (confirm("The monster beat you! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
   },
 });
